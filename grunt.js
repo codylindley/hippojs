fe3a6f8c4a1c17f8f2ea1bcc6429a9fd88ec176a
Cody Lindley <contact@codylindley.com>
2012-09-13 13:24:03 -0600

    updating qunit

--- grunt.js
+++ grunt.js
@@ -5,11 +5,7 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: '<json:package.json>',
     meta: {
-      banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-        '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
-        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
+      banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' + '<%= grunt.template.today("yyyy-mm-dd") %>\n' + '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' + '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' + ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
     },
     qunit: {
       files: ['test/**/*.html']
@@ -30,10 +26,13 @@ module.exports = function(grunt) {
       files: '<config:lint.files>',
       tasks: 'lint qunit'
     },
+    lint: {
+      afterconcat: ['dist/hippo.js']
+    },
     uglify: {}
   });
 
   // Default task.
-  grunt.registerTask('default', 'qunit concat min');
+  grunt.registerTask('default', 'qunit concat lint:afterconcat min');
 
 };

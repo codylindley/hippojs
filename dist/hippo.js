73e969f6e9418039f6283afa625195c3bb7e3422
Cody Lindley <contact@codylindley.com>
2012-09-13 10:27:54 -0600

    first commit

---
+++ dist/hippo.js
@@ -0,0 +1,67 @@
+/*! hippo - v1.0 - 2012-09-13
+* http://hippo.com
+* Copyright (c) 2012 ; Licensed  */
+
+(function(){
+
+//core.js
+//store reference to global context
+var global = this;
+
+//hippo func
+var hippo = function(selector) {
+	return new createHippoObject(selector);
+};
+
+var createHippoObject = function(selector) {
+	var nodes = document.querySelectorAll(selector);
+	for (var i = 0; i < nodes.length; i++) {
+		this[i] = nodes[i];
+	}
+	this.length = nodes.length;
+	return this;
+};
+
+//hippo version
+hippo.version = '1.0';
+
+if(typeof exports !== 'undefined'){//export for use on server i.e. node
+	exports.hippo = hippo;
+}else{//else assume browser
+	global.hippo = hippo;
+	if(!('$' in global)){
+		global.$ = hippo;
+	}
+}
+
+//prototype
+hippo.fn = createHippoObject.prototype = {
+    constructor:hippo
+};
+
+//utiliteis.js
+hippo.each = function(object, callback){
+	var name, i = 0, length = object.length;
+
+	for (; i < length;){
+		if (callback.call(object[i], i, object[i++]) === false){
+			break;
+		}
+	}
+
+	return object;
+};
+
+//looping.js
+hippo.fn.each = function(callback){
+    return hippo.each(this, callback);
+};
+
+//class.js
+hippo.fn.addClass = function(classString){
+	this.each(function(){
+		this.classList.add(classString);
+	});
+	return this;
+};
+}.call(this)); //call anynoumous function, set this value, for function to global scope
---
+++ grunt.js
@@ -0,0 +1,39 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: '<json:package.json>',
+    meta: {
+      banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+        '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
+        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
+    },
+    qunit: {
+      files: ['test/**/*.html']
+    },
+    concat: {
+      dist: {
+        src: ['<banner:meta.banner>', 'lib/intro.js', 'lib/core.js', 'lib/utilities.js', 'lib/looping.js', 'lib/manipulation/class.js', 'lib/outro.js'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    min: {
+      dist: {
+        src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
+        dest: 'dist/<%= pkg.name %>.min.js'
+      }
+    },
+    watch: {
+      files: '<config:lint.files>',
+      tasks: 'lint qunit'
+    },
+    uglify: {}
+  });
+
+  // Default task.
+  grunt.registerTask('default', 'qunit concat min');
+
+};
